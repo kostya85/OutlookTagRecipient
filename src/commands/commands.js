6ef6fb7b4671dcf568c6481e63cdb50cbb9a48d8
Konstantin <gorbunov.kostya@bk.ru>
2024-04-30 22:08:29 +0300

    GUID updated

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -180,11 +180,11 @@ function _tagExternal(hasExternal) {
     externalTag += typeof Office.context.mailbox.item.notificationMessages;
     externalTag += typeof Office.context.mailbox.item.notificationMessages.addAsync;
 
-    const id = 'test1';
+    const id = generateGuid();
     const details =
         {
-            type: Office.MailboxEnums.ItemNotificationMessageType.ProgressIndicator,
-            message: externalTag
+            type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+            message: 'В списке отправителей обнаружены внешние почтовые адреса'
         };
     Office.context.mailbox.item.notificationMessages.addAsync(id, details, () => {});
   } catch (err) {
@@ -290,6 +290,14 @@ function _tagExternal(hasExternal) {
       }
     );
   }
+}
+
+function generateGuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
 }
 
 // 1st parameter: FunctionName of LaunchEvent in the manifest; 2nd parameter: Its implementation in this .js file.
